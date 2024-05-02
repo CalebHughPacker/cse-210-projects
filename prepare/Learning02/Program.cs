8f74705ba57b00ed94ff91e6c532f8642b27174b
calebhpacker <packercaleb.h@gmail.com>
2024-05-02 17:38:02 -0600

    Week02 Prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Google";
+        job1._startYear = 1999;
+        job1._endYear = 2006;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Front-End Developer/Back-End Developer/Project Lead/Custodial Staff";
+        job2._company = "Joe's Tech Startup";
+        job2._startYear = 2006;
+        job2._endYear = 2007;
+
+        Resume resume = new Resume();
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume._name = "Jimmy Johnson";
+
+        resume.Display();
     }
 }
